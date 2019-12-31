6679f0881767015a12f58d44a05b440b76e95fc4
imhele <work@imhele.com>
2019-12-31 23:22:22 +0800

    hide nodes not shown in the page

--- wechat-minip/pages/components/icon/index.ts
+++ wechat-minip/pages/components/icon/index.ts
@@ -4,12 +4,14 @@ import Common from '../common';
 const app = getApp<App>();
 
 interface Data {
+  page: number;
   maskIcon?: string;
   maskVisible: boolean;
   navbarTitleVisible: boolean;
 }
 
 const initialData: Data = {
+  page: 0,
   maskVisible: false,
   navbarTitleVisible: false,
 };
@@ -26,5 +28,15 @@ Page({
   },
   hidePreview() {
     this.setData({ maskVisible: false });
+  },
+  onPageScroll(options: Page.IPageScrollOption) {
+    const page = Math.trunc(options.scrollTop / 168);
+    if (this.data.navbarTitleVisible) {
+      if (options.scrollTop < 64)
+        this.mutant().update({ navbarTitleVisible: this.data.navbarTitleVisible = false });
+    } else if (options.scrollTop > 70)
+      this.mutant().update({ navbarTitleVisible: this.data.navbarTitleVisible = true });
+    if (this.data.page !== page) this.mutant().update({ page });
+    this.mutant().commit();
   },
 });
