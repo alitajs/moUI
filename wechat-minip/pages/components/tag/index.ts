477090943bbc12ab5b9c8c3947b4633edf7c393f
imhele <work@imhele.com>
2020-01-02 17:08:38 +0800

    tag

---
+++ wechat-minip/pages/components/tag/index.ts
@@ -0,0 +1,21 @@
+import { App, EachPage } from '../../../app';
+import Common from '../common';
+
+const app = getApp<App>();
+
+interface Data {
+  navbarTitleVisible: boolean;
+}
+
+const initialData: Data = {
+  navbarTitleVisible: false,
+};
+
+Page({
+  ...Common,
+  ...(app.eachPage as EachPage<Data>),
+  data: { ...app.eachPage.data, ...initialData },
+  onLoad(query: Record<string, string>) {
+    this.onLoadOne(query, '标签').commit();
+  },
+});
